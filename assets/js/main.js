40c34a70d9e0e57ff0899b92574e293508eee990
AndreaDex <desiderio.andrea@outlook.it>
2021-05-17 15:13:08 +0200

    complete

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/* 
+ *Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+  todo Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data. 
+*/
+const app = new Vue({
+  el: "#app",
+  data: {
+    titolo: "Rembrandt - Cristo nella tempesta sul mare di Galilea",
+    imgPath:
+      "./assets/img/Rembrandt_Christ_in_the_Storm_on_the_Lake_of_Galilee.jpg",
+  },
+});
